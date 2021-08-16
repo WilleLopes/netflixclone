9935ac457b235feb1cfe29d6c64d382925c2b5f2
WilleLopes <willewowlopes@gmail.com>
2021-08-16 19:00:48 -0300

    pegando os de açao

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -39,7 +39,7 @@ export default {
             {
                 slug: 'action',
                 title: 'Ação',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
