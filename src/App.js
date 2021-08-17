a27e2ee8a2b15180749fbb082456f2dae45793bf
WilleLopes <willewowlopes@gmail.com>
2021-08-17 18:56:05 -0300

    css e filmes em destaque

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
-import './App.css'
+import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
-  const [movieList,  setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     const loadAll = async () => {
-     // Pegando a lista Total
-     let list = await Tmdb.getHomeList();
-     setMovieList(list);
+      // Pegando a lista Total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
 
     }
     loadAll();
-  },[])
+  }, [])
 
   return (
     <div className="page">
+
+      <FeaturedMovie />
+
       <section className="lists">
         {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items}/>
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
