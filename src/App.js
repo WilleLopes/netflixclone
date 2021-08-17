a48da81265bbec0986da4351576ddb9d17f03d66
WilleLopes <willewowlopes@gmail.com>
2021-08-16 21:33:54 -0300

    colocando a lista de filmes no console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  const [movieList,  setMovieList] = useState([]);
+
+  useEffect(() =>{
+    const loadAll = async () => {
+     // Pegando a lista Total
+     let list = await Tmdb.getHomeList();
+     console.log("essa é a lista de filmes", list)
+
+    }
+    loadAll();
+  },[])
+
   return (
     <div>
       olá mundo!
