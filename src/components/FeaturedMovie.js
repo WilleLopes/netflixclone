c35606e0f45b31537dd6ace4bf862d586d84c423
WilleLopes <willewowlopes@gmail.com>
2021-08-18 19:51:25 -0300

    pegando nomes de genero e fazendo header

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -4,10 +4,10 @@ import './FeaturedMovie.css';
 export default ({ item }) => {
     // console.log(item)
     let firstDate = new Date(item.first_air_date);
-    
+
     let genres = [];
-    for( let i in item.genres) {
-        genres.push( item.genres[i].name );
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
     }
 
     return (
@@ -26,7 +26,8 @@ export default ({ item }) => {
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
-
+                        <a href={`/watch/${item.id}`} className="featured--watchbutton"> Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
                     </div>
 
                     <div className="featured--genres">
